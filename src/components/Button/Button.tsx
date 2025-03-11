9784f2e3072eb24ecbd9f7c0a6bffc715636e00f
jpsalles21 <joaopedrosalles.dev@gmail.com>
2025-03-11 13:44:10 -0300

    wip: add button structure and create story

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,39 @@
+'use client'
+import styles from './Button.module.css'
+
+interface ButtonProps {
+    onClick?: (value: any) => any;
+    content: string;
+    variant: 'white' | 'dark';
+    width?: string;
+    height?: string;
+    disabled?: boolean;
+    type?: 'button' | 'submit' | 'reset' | undefined;
+}
+
+const Button = ({ onClick, content, variant, width, height, disabled = false, type }: ButtonProps) => {
+    
+ const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    if (disabled) {
+        event.preventDefault();
+        event.stopPropagation();
+        return;
+    }
+    if (onClick) {
+        onClick(event);
+    }
+}
+    return (
+        <button
+            className={styles[variant]}
+            onClick={handleClick}
+            type={type}
+            style={{ width: `${width}`, height: `${height}` }}
+            disabled={disabled}
+        >
+            {content}
+        </button>
+    );
+}
+
+export default Button;
