9cabee6221fa4bb90d7eba94097d0ce4b595e783
jpsalles21 <joaopedrosalles.dev@gmail.com>
2025-03-11 17:59:04 -0300

    fix: add HTML button properties

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,37 +1,33 @@
 'use client'
 import styles from './Button.module.css'
 
-interface ButtonProps {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     onClick?: (value: any) => any;
-    content: string;
     variant: 'white' | 'black';
     width?: string;
     height?: string;
-    disabled?: boolean;
-    type?: 'button' | 'submit' | 'reset' | undefined;
 }
 
-const Button = ({ onClick, content, variant, width, height, disabled = false, type }: ButtonProps) => {
-    
- const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    if (disabled) {
-        event.preventDefault();
-        event.stopPropagation();
-        return;
+const Button = ({ onClick, children, variant, width, height, disabled = false, type }: ButtonProps) => {
+
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        if (disabled) {
+            event.preventDefault();
+            event.stopPropagation();
+            return;
+        }
+        if (onClick) {
+            onClick(event);
+        }
     }
-    if (onClick) {
-        onClick(event);
-    }
-}
     return (
         <button
             className={styles[variant]}
             onClick={handleClick}
             type={type}
             style={{ width: `${width}`, height: `${height}` }}
-            disabled={disabled}
         >
-            {content}
+            {children}
         </button>
     );
 }
