cb90a76216d4081559a4636c12f6b1f05a55f071
jpsalles21 <joaopedrosalles.dev@gmail.com>
2025-03-12 14:09:50 -0300

    feat: add width and height as prop

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,15 +1,18 @@
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
     label: string,
     required: boolean;
+    width?: string;
+    height?: string; 
 }
 
-const Input = ({ label, type, placeholder, required }: InputProps) => {
+const Input = ({ label, type, placeholder, required, width, height }: InputProps) => {
     return (
         <div className="input">
             <label htmlFor="">{required && <span>*</span>}{label} </label>
             <input
                 type={type}
                 placeholder={placeholder}
+                style={{width, height}}
             />
         </div>
     );
