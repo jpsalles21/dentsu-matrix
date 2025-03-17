cfb37394684215b3e6f8a5d8931a6204efc79947
jpsalles21 <joaopedrosalles.dev@gmail.com>
2025-03-17 12:35:26 -0300

    wip: add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from './Footer.module.css'
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer__container}>© 2025 Dentsu. All Rights Reserved</footer>
+    );
+}
+
+export default Footer;
