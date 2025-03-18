f4135ea2ca0615ac8657b127ba53cb760d87583b
jpsalles21 <joaopedrosalles.dev@gmail.com>
2025-03-18 14:29:31 -0300

    wip: create bookingwidget component

---
+++ src/components/BookingWidget/BookingWidget.tsx
@@ -0,0 +1,12 @@
+const BookingWidget = () => {
+    return (
+        <section className="booking-widget">
+            <div className="booking-widget__container">
+                <h1>Reserve a vehicle</h1>
+                <p>Pick-up & Return Location (ZIP, City or Airport)</p>
+                <form action="">
+                </form>
+            </div>
+        </section>
+    );
+}
