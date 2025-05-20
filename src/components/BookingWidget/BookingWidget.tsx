239cc09546a4d98403e7ce8676dde0a4e855f41e
jpsallesds <joaopedro.salles@dentsu.com>
2025-05-20 15:11:57 -0300

    feat: add locations options

--- src/components/BookingWidget/BookingWidget.tsx
+++ src/components/BookingWidget/BookingWidget.tsx
@@ -1,15 +1,46 @@
 'use client'
+import { useState, useEffect } from "react";
 import { Input, Button } from "@components";
 import styles from './BookingWidget.module.css';
+import { getAllLocations } from "@/endpoints/services/location";
+import { Location } from "@/types/location";
 
 const BookingWidget = () => {
+    
+    const [locations, setLocations] = useState<Location[]>([]);
+    const [selectedLocation, setSelectedLocation] = useState('');
+
+    useEffect(() => {
+       const fetchLocations = async () => {
+            const data = await getAllLocations();
+            setLocations(data);
+            console.log(data);
+        }
+        fetchLocations();
+    }, []);
+
     return (
         <div className={styles.booking_widget__container}>
             <h1>Reserve a vehicle</h1>
-            <form action="" id="form1"> {/* temporary placeholder until the API is ready */}
+            <form action="" id="form1">
                 <div className={styles.booking_widget__container__inputs}>
                     <p>Pick-up & Return Location (ZIP, City or Airport)</p>
-                    <Input label="Required field" required={true} placeholder="Provide a Location" />
+                    <Input
+                        label="Required field"
+                        required={true}
+                        placeholder="Provide a Location"
+                        list="location-options"
+                        value={selectedLocation}
+                        onChange={e => setSelectedLocation(e.target.value)}
+                    />
+                    <datalist id="location-options">
+                        {locations.map(location => (
+                            <option
+                                key={location.id}
+                                value={`${location.name} - ${location.address}`}
+                            />
+                        ))}
+                    </datalist>
                 </div>
             </form>
             <div className={styles.booking_widget__container__actions}>
