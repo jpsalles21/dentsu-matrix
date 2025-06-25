eea4b107f0842c209d85e7aa1fa07d2c6d7e527c
João Pedro Salles <joaopedro.salles@dentsu.com>
2025-06-25 12:43:09 -0300

    chore: add headers components to index

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,3 +3,6 @@ export {default as Input} from "./Input/Input";
 export {default as Button} from "./Button/Button";
 export {default as Checkbox}  from "./Checkbox/Checkbox";
 export {default as BookingSteps} from "./BookingSteps/BookingSteps";
+export {default as Header} from "./Header/Header";
+export {default as SimpleHeader} from "./Header/SimpleHeader";
+export {default as BookingHeader} from "./BookingHeader/BookingHeader";
