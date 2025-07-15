1fc18349cbcabb15e862bb7278bf2ca1fb069f5f
João Pedro Salles <joaopedro.salles@dentsu.com>
2025-07-15 14:39:54 -0300

    refactor: add a default string to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import styles from './Footer.module.css'
 
 const Footer = ({children}: React.ComponentProps<"footer">) => {
     return (
-        <footer className={styles.footer__container}>{children}</footer>
+        <footer className={styles.footer__container}>{children || '© 2025 Dentsu. All Rights Reserved'}</footer>
     );
 }
 
